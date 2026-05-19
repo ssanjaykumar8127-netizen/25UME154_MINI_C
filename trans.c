a76b584e70b5725e1f9152684845b8756a10fd0d
ssanjaykumar8127-netizen <ssanjaykumar8127@gmail.com>
2026-05-19 22:55:30 +0530

    done

--- trans.c
+++ trans.c
@@ -109,7 +109,6 @@ int main(int argc, char *argv[])
 void textFile(FILE *readPtr)
 {
     FILE *writePtr; // accounts.txt file pointer
-    int result;     // used to test whether fread read any bytes
     // create clientData with default information
     struct clientData client = {0, "", "", 0.0};
 
@@ -123,18 +122,18 @@ void textFile(FILE *readPtr)
         rewind(readPtr); // sets pointer to beginning of file
         fprintf(writePtr, "%-6s%-16s%-11s%10s\n", "Acct", "Last Name", "First Name", "Balance");
 
-        // copy all records from random-access file into text file
-        while (!feof(readPtr))
+        // read exactly 100 records (faster and safer than feof loop)
+        for (int i = 0; i < 100; i++)
         {
-            result = fread(&client, sizeof(struct clientData), 1, readPtr);
+            fread(&client, sizeof(struct clientData), 1, readPtr);
 
-            // write single record to text file
-            if (result != 0 && client.acctNum != 0)
+            // write single record to text file if active
+            if (client.acctNum != 0)
             {
                 fprintf(writePtr, "%-6d%-16s%-11s%10.2f\n", client.acctNum, client.lastName, client.firstName,
                         client.balance);
             } // end if
-        }     // end while
+        } // end for
 
         fclose(writePtr); // fclose closes the file
     }                     // end else
